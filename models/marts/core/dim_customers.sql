77213639c2fc7b01f6123b5f4116921241fe96fa
SAURAV SAGAR <saurav.sagar0401@gmail.com>
2023-07-21 05:07:15 +0000

    final commit

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,13 +1,12 @@
 with customers as (
 
     select * from {{ ref('stg_customers') }}
-
+    
 ),
 
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -33,11 +32,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        orders.amount as lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    inner join orders using (customer_id)
 
 )
 
